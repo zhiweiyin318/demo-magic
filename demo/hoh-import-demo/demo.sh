ae5a830a487f1c2a99698e9d91da59db052a2ae7
Zhiwei Yin <zyin@redhat.com>
2022-11-03 23:22:57 +0800

    update demo

--- demo/hoh-import-demo/demo.sh
+++ demo/hoh-import-demo/demo.sh
@@ -38,63 +38,63 @@ clear
 # enters interactive mode and allows newly typed command to be executed
 cmd
 
-echo ""
+p ""
 p "Demo: Import an existing ACM Hub cluster with local-cluster as a regional hub cluster."
 
 
-echo ""
+p ""
 p "Step 1: Import the ACM Hub cluster in hosted mode."
 
-echo ""
+p ""
 p "Step 1.1: Add hosted annotations."
 p "Step 1.2: Disable all addons."
 p "Step 1.3: Import by kubeconfig mode."
 
-echo ""
+p ""
 pei "oc get mcl"
 
 pei "oc get klusterlet"
 
-echo ""
+p ""
 p "Step 2: Enable the work-manager addon in hosted mode."
-echo ""
+p ""
 
 p "Step 2.1: Create work-manager addon CR."
-echo ""
+p ""
 pei "cat work-manager-addon.yaml"
 pe "oc apply -f work-manager-addon.yaml"
 
-echo ""
+p ""
 p "Step 2.2: Create a kubeconfig secret for the work-manager addon."
-echo ""
+p ""
 pe "oc create secret generic work-manager-managed-kubeconfig --from-file=kubeconfig=./kubeconfig-hub1 -n open-cluster-management-hub1-addon-workmanager"
 
 pe "oc get managedclusteraddons -n hub1"
 pe "oc get pods -n open-cluster-management-hub1-addon-workmanager"
 
-echo ""
+p ""
 p "Step 3: Eanble multicluster-global-hub-controller addon." 
 
-echo ""
+p ""
 p "The multicluster-global-hub-controller addon CR will be created in default mode automatically by default."
 
-echo ""
+p ""
 pei "oc get managedclusteraddons -n hub1"
 
 pei "oc --kubeconfig=kubeconfig-hub1 get pods -n open-cluster-management-global-hub-system"
 
-echo ""
+p ""
 p "We can also enable the multicluster-global-hub-controller addon in hosted mode via a label."
-echo ""
+p ""
 
 pe "oc label mcl hub1 global-hub.open-cluster-management.io/agent-deploy-mode=Hosted --overwrite"
 
 pe "oc get managedclusteraddons -n hub1 multicluster-global-hub-controller -o yaml"
 
-echo ""
+p ""
 p "Need to create a kubeconfig secret for the HoH addon."
 
-echo ""
+p ""
 pe "oc create secret generic managed-kubeconfig-secret --from-file=kubeconfig=./kubeconfig-hub1 -n open-cluster-management-hub1-hoh-addon"
 
 pe "oc get managedclusteraddons -n hub1"
